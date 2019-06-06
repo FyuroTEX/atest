2543d355a4b5387dc6184afdba0f816277b0286c
FyuroTEX <yurilev4@gmail.com>
2019-06-06 21:13:16 +0300

    2 change

--- fTest.js
+++ fTest.js
@@ -22,15 +22,22 @@ suite(function(crm) {
       // driver.manage().setTimeouts({ implicit: 5000 });
     });
 
-    it('empty input field', async function () {
-      driver.manage().setTimeouts({ implicit: 5000 });
+    it('empty input field v1', async function () {
+     
+      await driver.get('https://rozetka.com.ua/');
+   
+      let awButtn = driver.wait(until.elementLocated(By.css('a.header-topline__user-link')), 4000);
+      await awButtn.click();
+      let loginBttn = driver.wait(until.elementLocated(By.css('button.auth-modal__login-button'), 5000));
+      await loginBttn.click();
+      
+      await driver.findElement(By.css('#auth_email.form_state_error'));
+    });
+    it('empty input field v2', async function () {
+      driver.manage().setTimeouts({ implicit: 10000 });
       await driver.get('https://rozetka.com.ua/');
       await driver.findElement(By.css('a.header-topline__user-link')).click();
-
-      // await driver.wait(until.elementLocated(By.css('button.auth-modal__login-button'), 5000)).click();
-
-      // await driver.wait(until.elementLocated(By.css('#auth_email.form_state_error'), 5000));
-
+      
       await driver.findElement(By.css('button.auth-modal__login-button')).click();
 
       await driver.findElement(By.css('#auth_email.form_state_error'));

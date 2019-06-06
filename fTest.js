05d934b59ff70669520ac2656977680df50212d3
FyuroTEX <yurilev4@gmail.com>
2019-06-06 21:15:44 +0300

    aa

--- fTest.js
+++ fTest.js
@@ -42,37 +42,7 @@ suite(function(crm) {
 
       await driver.findElement(By.css('#auth_email.form_state_error'));
     });
-    // it('invalid email', async function() {
-    //   await driver.get('https://rozetka.com.ua/');
-    //   await driver.findElement(By.css('.header-topline__user-link')).click();
-    //   await driver
-    //     .findElement(By.id('#auth_email'))
-    //     .sendKeys('0000@gmail.com', Key.RETURN);
-
-    //   await driver
-    //     .wait(
-    //       until.elementLocated(By.css('button.auth-modal__login-button'), 4000)
-    //     )
-    //     .click();
-    //     await driver.wait(
-    //       until.elementLocated(By.css('#auth_pass.form_state_error'), 1000)
-    //     );
-    // });
-
-    // it('webdriver - Google Search', async function() {
-    //   await driver.get('https://www.google.com/ncr');
-    //   await driver.findElement(By.name('q')).sendKeys('webdriver');
-    //   await driver.findElement(By.name('btnK')).click();
-    //   // await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-    //   await driver.getTitle('webdriver - Google Search');
-    // });
-
-    // ignore(crm.browsers(Browser.CHROME)).
-    // it('demo 2', async function() {
-    //   await driver.get('http://www.google.com/ncr');
-    //   let url = await driver.getCurrentUrl();
-    //   assert.equal(url, 'https://www.google.com/');
-    // });
+    
 
     after(() => driver && driver.quit());
   });
